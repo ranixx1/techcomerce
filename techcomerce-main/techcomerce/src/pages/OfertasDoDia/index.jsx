450d50b6334d168c41163df5ae6f6c5f95a3bbce
ranilton <ranilton0706@gmail.com>
2025-01-28 21:48:41 -0300

    finalizando

--- techcomerce-main/techcomerce/src/pages/OfertasDoDia/index.jsx
+++ techcomerce-main/techcomerce/src/pages/OfertasDoDia/index.jsx
@@ -1,6 +1,13 @@
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+
 function OfertasDoDia(){
     return (
+        <>
+        <Header />
         <h1>As melhores Ofertas estão aqui</h1>
+        <Footer />
+        </>
     );
 }
 export default OfertasDoDia;
